60a270cea1292f64fa10778d80052f077f7cd897
QwejoD <desmondqwejo@gmail.com>
2023-05-06 12:58:33 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""A function that writes a string to a text file (UTF8) and returns the number of characters written"""
+"""
+function that writes a string to a text file (UTF8) and returns the number of characters written
+"""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="utf-8") as f:
+    """ module write_file
+    """
+    with open(filename, 'w') as f:
         return f.write(text)
-
