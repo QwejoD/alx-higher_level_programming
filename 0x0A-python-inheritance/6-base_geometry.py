f578476fdc9f5670818cad215155788b495a31c8
QwejoD <desmondqwejo@gmail.com>
2023-05-08 01:24:59 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining class"""
+
+
+class BaseGeometry:
+    """The class base geometry."""
+
+    def area(self):
+        """raise and exception"""
+        raise Exception("area() is not implemented")
