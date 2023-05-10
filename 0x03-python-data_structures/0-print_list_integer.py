c7dae7914777b43472b93d379fd86b6e3191a12a
Desmond Osarfo Amoah <desmondqwejo@gmail.com>
2023-05-10 17:47:40 +0000

    delete

--- 0x03-python-data_structures/0-print_list_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''A function that prints all integers in a list'''
-
-'''function definition'''
-def print_list_integer(my_list=[]):
-
-    '''print integer'''
-    for i in my_list:
-        print("{:d}".format(int(i)))
--- 0x03-python-data_structures/README.md
+++
@@ -1,1 +0,0 @@
-Readme for 0x03. Python - Data Structures: Lists, Tuples
