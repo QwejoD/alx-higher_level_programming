af99454e6712ec800ddcf547ff3b80e0a609e962
QwejoD <desmondqwejo@gmail.com>
2023-05-14 17:33:10 -0700

    advacned 2

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes
 """
+
+
 class LockedClass:
+    """locked class"""
 
     __slots__ = "first_name"
