bf4c7e2ebecd489590586d98f3c38eab2bf8ca68
QwejoD <desmondqwejo@gmail.com>
2023-05-17 21:25:40 -0700

    task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- Displays table info without using DESCRIBE or EXPLAIN
+SHOW CREATE TABLE first_table;
