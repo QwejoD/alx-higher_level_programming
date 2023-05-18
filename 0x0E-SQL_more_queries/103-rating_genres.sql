9d6a9a33a50785a286ecc69ca2b801f8ba67bf4b
QwejoD <desmondqwejo@gmail.com>
2023-05-18 02:39:22 -0700

    advanced

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Sorted in descending order
+SELECT g.name, SUM(r.rate) AS rating
+FROM tv_genres g INNER JOIN tv_show_genres sg
+  ON g.id = sg.genre_id
+  INNER JOIN tv_show_ratings r
+  ON sg.show_id = r.show_id
+GROUP BY g.name
+ORDER BY SUM(r.rate) desc;
