45958cf2f139eefb020375247082898192894bdf
QwejoD <desmondqwejo@gmail.com>
2023-05-17 21:58:11 -0700

    task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Lists all records of the table second_table of the database
+-- List rows only where `name` is not NULL
+SELECT score, name FROM `second_table` WHERE NAME IS NOT NULL ORDER BY score desc;
