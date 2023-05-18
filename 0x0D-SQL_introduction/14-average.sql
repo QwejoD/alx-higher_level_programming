1d4630758c7dbde965112379e1711b6d862f8cfd
QwejoD <desmondqwejo@gmail.com>
2023-05-17 21:52:41 -0700

    task 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Computes the score average of all records in the table second_table of the database
+-- The name of the results column is Average
+SELECT AVG(score) AS average FROM `second_table`;
