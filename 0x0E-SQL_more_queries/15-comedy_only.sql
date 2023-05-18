f39d263e6823ff957e8b97b0b4fd6c1330e0858e
QwejoD <desmondqwejo@gmail.com>
2023-05-18 02:24:59 -0700

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows.
+-- Sorted in ascedning order
+SELECT s.title
+FROM tv_genres g INNER JOIN tv_show_genres sg
+  ON g.id = sg.genre_id
+  INNER JOIN tv_shows s
+  ON sg.show_id = s.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title;
