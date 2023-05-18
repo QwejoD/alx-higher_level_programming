95b01d8c3085854818de08566306a3c98ae54c33
QwejoD <desmondqwejo@gmail.com>
2023-05-17 21:22:46 -0700

    task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table called first_table in the current database in your MySQL server
+-- Creates a table in `hbtn_0c_0` database
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
