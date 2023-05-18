9e29b8a953372b3fe8a5c226f841a2e23922d403
QwejoD <desmondqwejo@gmail.com>
2023-05-18 02:10:20 -0700

    task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Sorted in ascending order
+SELECT s.title AS title, g.genre_id AS genre_id
+FROM tv_shows s LEFT OUTER JOIN tv_show_genres g
+  ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
