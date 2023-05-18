447eb78ce59c5a226f59082ba1b7a5130df99dc7
QwejoD <desmondqwejo@gmail.com>
2023-05-18 01:16:33 -0700

    task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- privileges of user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- privileges of user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
