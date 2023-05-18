420cf3400c2b89d54bc3e1103118e482ea9968b0
QwejoD <desmondqwejo@gmail.com>
2023-05-17 21:34:21 -0700

    task 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Displays the number of records with id = 89 in the table first_table of the database
+-- Database name is passed as an argument of the mysql command
+SELECT COUNT(*) FROM `first_table` WHERE `id` = 89;
